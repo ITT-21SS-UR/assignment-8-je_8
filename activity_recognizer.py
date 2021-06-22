aea4042b16fa383925cbe3bf09409c13591912f8
joshua benker <joshuabenker@joshuas-MBP.fritz.box>
2021-06-22 21:02:50 +0200

    training done

--- activity_recognizer.py
+++ activity_recognizer.py
@@ -62,7 +62,16 @@ fclib.registerNodeType(DisplayTextNode, [('display',)])
 class SvmNode(Node):
     nodeName = 'svm'
 
+    INACTIVE_TEXT = "the node is inactive. choose 'prediction'-mode to predict a gesture" \
+                    " or 'training'-mode to train a new gesture"
+    PREDICTION_TEXT = "press 'start' and execute a gesture." \
+                      "press 'stop' when done and the category will be predicted"
+    TRAINING_TEXT = "select an activtiy in the list and record performing the gesture" \
+                    " by pressing the record button"
+
     state = GestureNodeState.INACTIVE
+    gestures_dict = {}
+    gesture_id = 0 
 
     def __init__(self, name):  
         Node.__init__(self, name, terminals={  
@@ -71,37 +80,41 @@ class SvmNode(Node):
             })
         self.prediction = '' 
         # example gestures- add more?
-        self.saved_activities = ['jump', 'run', 'throw'] 
+        self.saved_gestures = ['jump', 'run', 'throw'] 
         self.is_recording = False
         #svm
-        self.svc = svm.SVC()
+        self.svc = svm.SVC(kernel='rbf')
         self.init_ui()
 
     def init_ui(self):
         self.ui = QtGui.QWidget()
         self.layout = QtGui.QVBoxLayout()
         self.mode_layout = QtGui.QGridLayout()
-        self.activity_layout = QtGui.QGridLayout()
-        self.activity_widg = QtGui.QWidget()
+        self.gesture_layout = QtGui.QGridLayout()
+        self.pred_layout = QtGui.QGridLayout()
+        self.gesture_widg = QtGui.QWidget()
+        self.pred_widg = QtGui.QWidget()
 
         # init buttons for the mode: 'inactive', 'training' or 'predicting'
         self.init_mode_ui()
 
         # mode instructions
         self.mode_text_label = QtGui.QLabel()
-        self.mode_text_label.setText("the node is inactive. choose 'prediction'-mode to predict a activity"  
-                                            " or 'training'-mode to train a new activity")
+        self.mode_text_label.setText(self.INACTIVE_TEXT)
         self.mode_layout.addWidget(self.mode_text_label, 1, 0, 3, 3)
-        self.activity_name = QtGui.QLineEdit()
-        self.activity_name.setVisible(False)
-        self.mode_layout.addWidget(self.activity_name, 7, 0, 2, 2)
+        self.gesture_name = QtGui.QLineEdit()
+        self.gesture_name.setVisible(False)
+        self.mode_layout.addWidget(self.gesture_name, 7, 0, 2, 2)
 
         self.init_training_ui()
+        self.init_prediction_ui()
         
-        self.activity_widg.setLayout(self.activity_layout)
+        self.gesture_widg.setLayout(self.gesture_layout)
+        self.pred_widg.setLayout(self.pred_layout)
         self.layout.addLayout(self.mode_layout)
-        self.layout.addWidget(self.activity_widg)
-        self.activity_widg.setVisible(False)
+        self.layout.addWidget(self.gesture_widg)
+        self.layout.addWidget(self.pred_widg)
+        self.pred_widg.setVisible(False)
         self.ui.setLayout(self.layout)
 
     def init_mode_ui(self):
@@ -124,27 +137,27 @@ class SvmNode(Node):
 
 
     def init_training_ui(self):
-        new_activity_label = QtGui.QLabel("create a new activity")
-        self.activity_layout.addWidget(new_activity_label)
-        self.activity_name = QtGui.QLineEdit()
-        self.activity_layout.addWidget(self.activity_name,7,0)
+        new_gesture_label = QtGui.QLabel("create a new gesture")
+        self.gesture_layout.addWidget(new_gesture_label)
+        self.gesture_name = QtGui.QLineEdit()
+        self.gesture_layout.addWidget(self.gesture_name,7,0)
 
-        self.activity_select = QtWidgets.QComboBox()
-        self.activity_select.addItems(self.saved_activities)
-        self.add_button = QtGui.QPushButton('add actvity')
-        edit_activity_label = QtGui.QLabel("edit activity")
-        self.train_button = QtGui.QPushButton('train activity')
-        self.delete_button = QtGui.QPushButton("delete activity")
+        self.gesture_select = QtWidgets.QComboBox()
+        # self.gesture_select.addItems(self.saved_gestures)
+        self.add_button = QtGui.QPushButton('add gesture')
+        edit_gesture_label = QtGui.QLabel("edit gesture")
+        self.train_button = QtGui.QPushButton('train gesture')
+        self.delete_button = QtGui.QPushButton("delete gesture")
         self.record_button = QtGui.QPushButton("start recording")
         self.stop_record_button = QtGui.QPushButton("stop recording")
 
-        self.activity_layout.addWidget(self.add_button, 7,1)
-        self.activity_layout.addWidget(edit_activity_label)
-        self.activity_layout.addWidget(self.activity_select,9,0)
-        self.activity_layout.addWidget(self.train_button,9,1)
-        self.activity_layout.addWidget(self.delete_button,9,2)
-        self.activity_layout.addWidget(self.record_button,10,1)
-        self.activity_layout.addWidget(self.stop_record_button,10,2)
+        self.gesture_layout.addWidget(self.add_button, 7, 1)
+        self.gesture_layout.addWidget(edit_gesture_label)
+        self.gesture_layout.addWidget(self.gesture_select, 9, 0)
+        self.gesture_layout.addWidget(self.train_button, 9, 1)
+        self.gesture_layout.addWidget(self.delete_button, 9, 2)
+        self.gesture_layout.addWidget(self.record_button, 10, 1)
+        self.gesture_layout.addWidget(self.stop_record_button, 10, 2)
         self.record_button.hide()
         self.stop_record_button.hide()
 
@@ -155,59 +168,123 @@ class SvmNode(Node):
         self.stop_record_button.clicked.connect(self.on_stop_record_button_clicked)
 
 
-    #prediction ui: start and stop button
+    # prediction ui: start and stop button
     def init_prediction_ui(self):
-        pass
+        new_pred_label = QtGui.QLabel("predict a gesture")
+        self.pred_layout.addWidget(new_pred_label)
+        self.pred_start_button = QtGui.QPushButton("start predicting")
+        self.pred_stop_button = QtGui.QPushButton("stop predicting")
 
-    # new activity is added to the list
+        self.pred_layout.addWidget(self.pred_start_button, 11, 0)
+        self.pred_layout.addWidget(self.pred_stop_button, 11, 1)
+
+        self.pred_start_button.clicked.connect(self.on_pred_start_button_clicked)
+        self.pred_stop_button.clicked.connect(self.on_pred_stop_button_clicked)
+
+
+    # new gesture is added to the list
     def on_add_button_clicked(self):
-        self.saved_activities.append(self.activity_name.text())
-        self.activity_name.setText("")
-        self.activity_select.addItem(self.saved_activities[-1])
+        self.saved_gestures.append(self.gesture_name.text())
+        self.gesture_select.addItem(self.saved_gestures[-1])
+        #self.gestures_dict[self.gesture_id] = {}
+        self.gestures_dict[self.gesture_name.text()] = []
+        self.gesture_name.setText("")
+        self.gesture_id += 1
+
 
     def on_train_button_clicked(self):
         self.record_button.show()
         self.stop_record_button.show()
 
     def on_record_button_clicked(self):
-        self.is_recording = True
+        self.activity_recording(True)
     
     def on_stop_record_button_clicked(self):
-        self.is_recording = False
+        self.activity_recording(False)
+    
+    def activity_recording(self, is_recording):
+        self.is_recording = is_recording
+        if self.state == GestureNodeState.TRAINING:
+            if self.is_recording:
+                self.mode_text_label.setText("Recording...")
+            else:
+                self.mode_text_label.setText(self.TRAINING_TEXT)
+
 
     def on_delete_button_clicked(self):      
-        print("delete button clicked")
+        gesture_selected = self.gesture_select.currentText()
+        self.saved_gestures.remove(gesture_selected)
+        self.gesture_select.clear()
+        self.gesture_select.addItems(self.saved_gestures)
+
+    def on_pred_start_button_clicked(self):
+        self.is_recording = True
+        
+    def on_pred_stop_button_clicked(self):
+        self.is_recording = False
+    
 
     def on_mode_button_clicked(self, buttonType):
         if buttonType is self.training_button:
             self.state = GestureNodeState.TRAINING
-            self.mode_text_label.setText("select an activtiy in the list and record performing the activity" 
-                                                "by pressing the record button")
-            self.activity_name.setText('')
-            self.activity_widg.setVisible(True)
-
+            self.mode_text_label.setText(self.TRAINING_TEXT)
+            self.gesture_name.setText('')
+            self.gesture_widg.setVisible(True)
+            self.pred_widg.setVisible(False)
         elif buttonType is self.prediction_button:
             self.state = GestureNodeState.PREDICTING
-            self.activity_widg.setVisible(False)
-            self.mode_text_label.setText("press 'button 1' and execute an acitivity. after releasing it " 
-                                            " it will predict your activity")
-            self.init_prediction_ui()
-
+            self.gesture_widg.setVisible(False)
+            self.pred_widg.setVisible(True)
+            self.mode_text_label.setText(self.PREDICTION_TEXT)
         else:
             self.state = GestureNodeState.INACTIVE
-            self.activity_widg.setVisible(False)
-            self.mode_text_label.setText("the node is inactive. choose 'prediction'-mode to predict a activity"  
-                                            " or 'training'-mode to train a new activity")
+            self.pred_widg.setVisible(False)
+            self.gesture_widg.setVisible(False)
+            self.mode_text_label.setText(self.INACTIVE_TEXT)
 
     def ctrlWidget(self):  
         return self.ui
 
-    def predict_activity(self, kargs):
-        self.prediction = self.svc.predict(kargs['dataIn'])
-        print(self.prediction)
+    def handle_gesture_training(self, kargs):
+        if self.is_recording:
+            input_val = kargs['dataIn']
+            selected_gesture = self.gesture_select.currentText()
+            print(self.gestures_dict)
+            self.gestures_dict[selected_gesture].append(input_val)
+        else:
+            samples = []
+            targets = []
+        
+            for key in self.gestures_dict:
+                for feature in self.gestures_dict[key]:
+                    feature = feature.flatten()
+                    samples.append(feature)
+                    targets.append(key)
+
+            if not all(f == targets[0] for f in targets):
+                self.svm.fit(samples, targets)
+            
+
+    def predict_gesture(self, kargs):
+        if self.is_recording:
+            try: 
+                input_val = kargs['dataIn']
+                self.prediction = self.svc.predict(input_val)
+            except NotFittedError:
+                return
+            for key in self.gestures_dict:
+                if key == prediction[0]:
+                    return {'prediction': self.gestures_dict[key]}
 
     def process(self, **kargs):
-        return {'frequency': self.predict}
+        self.output = {'dataIn': "-"}
+        if self.state == GestureNodeState.TRAINING:
+            self.handle_gesture_training(kargs)
+
+        if self.state == GestureNodeState.PREDICTING:
+            self.output = self.predict_gesture(kargs)
+
+        return self.output
 
 fclib.registerNodeType(SvmNode, [('svm',)])
 
@@ -223,18 +300,14 @@ class FftNode(Node):
             'accelZ': {'io': 'in'},
             'frequency': {'io': 'out'},
             })
+        self.frequency = None
 
-    def calc_frequency(self, val):
-        return [np.abs(np.fft.fft(val) / len(values))[1:len(val) // 2]]
-
-    def process(self, **kwds):
-        fft_x = self.calc_frequency(kwargs[accelX.value])
-        fft_y = self.calc_frequency(kwargs[accelY.value])
-        fft_z = self.calc_frequency(kwargs[accelZ.value])
-        fft = np.array([fft_x, fft_y, fft_z])
-
-        # not sure. maybe only fft_x?
-        return {'frequency': fft} 
+    def process(self, **kargs):
+        avg = []
+        for i in range(len(kargs['accelX'])):
+            avg.append((kargs['accelX'][i] + kargs['accelY'][i] + kargs['accelZ'][i]) / 3)
+        self.frequency = np.abs(np.fft.fft(avg) / len(avg))[1:len(avg) // 2]
+        return {'frequency': self.frequency}
 
 fclib.registerNodeType(FftNode, [('fft',)])
 
@@ -245,8 +318,8 @@ def create_connect_nodes(chart):
     buffer_node_y = chart.createNode("Buffer", pos=(100, 0))
     buffer_node_z = chart.createNode("Buffer", pos=(100, 200))
     fft_node = chart.createNode("fft", pos=(200, 100))
-    svm_node = chart.createNode("svm", pos=(300, 0))
     display_node = chart.createNode("display", pos=(400,0))
+    svm_node = chart.createNode("svm", pos=(300, 0))
 
     chart.connectTerminals(dippid_node['accelX'], buffer_node_x['dataIn'])
     chart.connectTerminals(dippid_node['accelY'], buffer_node_y['dataIn'])
@@ -254,6 +327,8 @@ def create_connect_nodes(chart):
     chart.connectTerminals(buffer_node_x['dataOut'], fft_node['accelX'])
     chart.connectTerminals(buffer_node_y['dataOut'], fft_node['accelY'])
     chart.connectTerminals(buffer_node_z['dataOut'], fft_node['accelZ'])
+    chart.connectTerminals(fft_node['frequency'], svm_node['dataIn'])
+    chart.connectTerminals(svm_node['prediction'], display_node['dataIn'])
 
 def start():
     app = QtWidgets.QApplication([])
